98262a97d4e78ce0a983fd213229a4a118b9fc33
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-11-10 09:51:21 +0100

    static

--- src/com/kristoffer/demo/Main.java
+++ src/com/kristoffer/demo/Main.java
@@ -3,5 +3,23 @@ package com.kristoffer.demo;
 public class Main {
     public static void main(String[] args) {
 
+        // Instantiate
+        Counter counterA = new Counter();
+        Counter counterB = new Counter();
+        Counter counterC = new Counter();
+
+        counterA.increaseCount();   // 0 -> 1
+        counterB.increaseCount();   // 1 -> 2
+        counterC.increaseCount();   // 2 -> 3
+
+        // TODO - Static Method, does not require instantiation to invoke method
+        Counter.sayHello();
+
+        // TODO - Counter DOES NOT have Static Currently
+        // What's going to happen when we run the code now?
+
+        // TODO - Why does each counter start with 0?
+        // Memory positions, they are NOT shared
+
     }
 }
