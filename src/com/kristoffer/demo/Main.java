d6f7c33ae5287046d2bd1eba90d11c9bf5ac49c0
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-11-10 10:15:31 +0100

    example

--- src/com/kristoffer/demo/Main.java
+++ src/com/kristoffer/demo/Main.java
@@ -1,7 +1,30 @@
 package com.kristoffer.demo;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+
+        // TODO - What can we say about 'List'?
+        List<Integer> scoreList = new ArrayList<>(15);
+        List<String> test2 = new LinkedList<>();
+
+        // TODO - LinkedList vs ArrayList...?
+        // TODO - LinkedList: pointers! (from left to right)
+        // TODO - ArrayList: initialCapacity (x1.5)
+        // Deck OF Cards - 52
+
+        // TODO - Why does this not print values?
+        scoreList.add(500);         // Add value 500
+        scoreList.get(0);           // Get index 0 -> 500
+        scoreList.set(0, 1000);     // Change index 0 -> 500 = 1000
+        scoreList.get(0);           // Get index 0 -> 1000
+        scoreList.remove(0);   // Remove index 0 -> 1000
+
+        System.out.println(scoreList);
 
     }
 }
